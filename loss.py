ad7f42eab694165b49dddc8263acb8adf6dc4dac
Piotr Maślankowski <pmaslankowski@gmail.com>
2019-06-14 23:53:32 +0200

    WIP - first working version.

---
+++ loss.py
@@ -0,0 +1,41 @@
+import imageio
+import numpy as np
+from time import time
+
+from scipy.misc import imresize
+from utils import construct_image
+
+
+class SquaredDifferenceLoss(object):
+
+    def __init__(self, target_image_path, internal_shape=(75, 75), k=3, verbose=True):
+        self.target_image = imresize(imageio.imread(target_image_path), internal_shape)
+        self.verbose = verbose
+        self.k = k
+
+    def __call__(self, population):
+        population_size = population.shape[0]
+        result = np.empty(population_size, dtype=np.float64)
+        height, width, depth = self.target_image.shape
+
+        start_timestamp = time()
+        for i in range(population_size):
+            image = construct_image(population[i], (height, width), self.k)
+            result[i] = self.pixelwise_diff(image, self.target_image)
+        end_timestamp = time()
+        time_elapsed = end_timestamp - start_timestamp
+        if self.verbose:
+            print('[SquaredDifferenceLoss]: Processing batch of ' + str(population_size) + ' individuals took ' +
+                  str(time_elapsed) + ' s.')
+        return result
+
+    @staticmethod
+    def pixelwise_diff(generated_image, target_image):
+        '''
+        Returns summed pixelwise difference between generated and target image.
+        :param generated_image: shape - (h, w, 4) - generated image in rgba format
+        :param target_image: shape - (h, w, 4) - target image in rgba format
+        :return: pixelwise difference between given images
+        '''
+
+        return np.sum((generated_image.astype(np.int64) - target_image.astype(np.int64)) ** 2)
---
+++ utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cairo
+from matplotlib import pyplot as plt
+
+
+def construct_image(polygons, image_shape, k):
+    '''
+    Constructs image from circles description.
+    :param circles: shape - (population_size, 4 + 2*k) - Description of circles. Each row contains
+    r, g, b, a, x1, y1, ..., xk, yk where:
+    :param image_shape - (height, width) - shape of constructed image
+    :return: image of shape image_shape generated with given circles
+    '''
+
+    height, width = image_shape
+    data = np.zeros((height, width, 4), dtype=np.uint8)
+    surface = cairo.ImageSurface.create_for_data(data, cairo.FORMAT_ARGB32, height, width)
+    cr = cairo.Context(surface)
+    #cr.paint()
+    for polygon in polygons:
+        cr.set_source_rgba(polygon[0], polygon[1], polygon[2], polygon[3])
+        cr.move_to(polygon[4] * width, polygon[5] * height)
+        for j in range(1, k):
+            cr.line_to(polygon[4 + 2 * j] * width, polygon[4 + 2 * j + 1] * height)
+        cr.fill()
+    return data
+
+
+if __name__ == '__main__':
+    polygons = np.array([[1.0, 0., 0., 1., 30, 30, 30, 50, 50, 50],
+                         [0.0, 1.0, 0., 0.5, 40, 40, 80, 50, 90, 100],
+                         [0., 0., 1., 0.3, 10, 10, 120, 10, 100, 150]])
+    img = construct_image(polygons, (200, 200), 3)
+    plt.imshow(img)
+    plt.show()
